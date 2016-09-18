9283768ffb7a9e957d6002309227ad363094bde1
MarvinMenjivar <eduardo.menjivar92@gmail.com>
2016-09-17 21:14:36 -0600

    Agregar manejo de excepciones y mensajes al usuario

--- Editor/Editor/Form1.Designer.vb
+++ Editor/Editor/Form1.Designer.vb
@@ -103,7 +103,7 @@ Partial Class Form1
         Me.EdicionToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.DeshacerToolStripMenuItem, Me.RehacerToolStripMenuItem, Me.CortarToolStripMenuItem, Me.CopiarToolStripMenuItem, Me.PegarToolStripMenuItem, Me.SeleccionarTodoToolStripMenuItem, Me.LimpiarTexoToolStripMenuItem})
         Me.EdicionToolStripMenuItem.Name = "EdicionToolStripMenuItem"
         Me.EdicionToolStripMenuItem.Size = New System.Drawing.Size(58, 20)
-        Me.EdicionToolStripMenuItem.Text = "Edicion"
+        Me.EdicionToolStripMenuItem.Text = "Edición"
         '
         'DeshacerToolStripMenuItem
         '
@@ -171,13 +171,13 @@ Partial Class Form1
         '
         Me.LeemeToolStripMenuItem.Name = "LeemeToolStripMenuItem"
         Me.LeemeToolStripMenuItem.Size = New System.Drawing.Size(152, 22)
-        Me.LeemeToolStripMenuItem.Text = "Leeme"
+        Me.LeemeToolStripMenuItem.Text = "Léeme"
         '
         'ParametrosToolStripMenuItem
         '
         Me.ParametrosToolStripMenuItem.Name = "ParametrosToolStripMenuItem"
         Me.ParametrosToolStripMenuItem.Size = New System.Drawing.Size(79, 20)
-        Me.ParametrosToolStripMenuItem.Text = "Parametros"
+        Me.ParametrosToolStripMenuItem.Text = "Parámetros"
         '
         'ToolStrip1
         '
